f22cb228672f53e1fda3b954fb5d8c68e9013802
Aditya Jaiswal <51635762+Adityajl@users.noreply.github.com>
2026-07-03 13:27:08 +0530

    Time: 18 ms (100%), Space: 239.1 MB (99.6%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,77 +1,52 @@
+constexpr long long INF = 100'000'000'000'000;
+int nbi[50001], srt[50000];
+long long dis[50000];
+char vis[50000];
+pair<int,int> nbs[100000];
+
+void tSort(int n, int &p) {
+    if (vis[n]) return; else vis[n] = 1;
+    for (int i = nbi[n], e = nbi[n+1]; i != e; ++i) tSort(nbs[i].first, p);
+    if (p || !n) srt[p++] = n;
+}
+
+bool eval(int n, int x, long long k) {
+    dis[0] = 0;
+    for (int i = 1; i < n; ++i) {
+        int nd = srt[i];
+        long long dmin = INF;
+        for (int j = nbi[nd], e = nbi[nd+1]; j != e; ++j) if (nbs[j].second >= x) {
+            dmin = min(dmin, dis[nbs[j].first] + nbs[j].second);
+        }
+        dis[nd] = dmin;
+    }
+    return dis[srt[n-1]] <= k;
+}
+
 class Solution {
 public:
     int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
-        int n = online.size();
+        int N = size(online);
+        fill(nbi, nbi+N, 0);
+        for (const auto &e : edges) if (online[e[0]] && online[e[1]]) ++nbi[e[1]];
+        for (int i = 1; i < N; ++i) nbi[i] += nbi[i-1];  nbi[N] = nbi[N-1];
+        for (const auto &e : edges) if (online[e[0]] && online[e[1]]) nbs[--nbi[e[1]]] = { e[0], e[2] };
 
-        vector<vector<pair<int,int>>> graph(n);
-        vector<int> indegree(n, 0);
+        int n=0;
+        fill(vis, vis+N, 0); tSort(N-1, n);
+        //for (int i = 0; i < n; ++i) cout << srt[i] << " "; cout << endl;
+        fill(dis, dis+N, INF);
+        if (!(n && eval(n,0,k))) return -1;
 
-        for (auto &e : edges) {
-            graph[e[0]].push_back({e[1], e[2]});
-            indegree[e[1]]++;
+        int lo=INT_MAX,hi=0;
+        for (int i = 0, e = nbi[N]; i != e; ++i) {
+            int d = nbs[i].second;
+            lo = min(lo, d); hi = max(hi, d);
         }
-
-        queue<int> q;
-        for (int i = 0; i < n; i++)
-            if (indegree[i] == 0)
-                q.push(i);
-
-        vector<int> topo;
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            topo.push_back(u);
-
-            for (auto &[v, w] : graph[u]) {
-                if (--indegree[v] == 0)
-                    q.push(v);
-            }
+        while (lo < hi) {
+            int m = (lo + hi + 1)/2;
+            if (eval(n,m,k)) lo = m; else hi = m-1;
         }
-
-        auto check = [&](int limit) {
-            const long long INF = (1LL << 60);
-
-            vector<long long> dp(n, INF);
-            dp[0] = 0;
-
-            for (int u : topo) {
-
-                if (dp[u] == INF)
-                    continue;
-
-                if (u != 0 && u != n - 1 && !online[u])
-                    continue;
-
-                for (auto &[v, w] : graph[u]) {
-
-                    if (w < limit)
-                        continue;
-
-                    if (v != n - 1 && !online[v])
-                        continue;
-
-                    if (dp[u] + w < dp[v])
-                        dp[v] = dp[u] + w;
-                }
-            }
-
-            return dp[n - 1] <= k;
-        };
-
-        int left = 0, right = 1000000000;
-        int ans = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (check(mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return ans;
+        return lo;
     }
 };
